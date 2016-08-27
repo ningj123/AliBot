2742b82c9474eb1c6e032c1fc34285a7995e68da
fieldyslap <fieldyslap@yandex.ru>
2016-08-28 01:30:36 +0700

    Main Source

--- AliBot/Form1.Designer.cs
+++ AliBot/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenBrowser = new System.Windows.Forms.Button();
+            this.CloseBrowser = new System.Windows.Forms.Button();
+            this.FileSelectorURLs = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenBrowser
+            // 
+            this.OpenBrowser.Location = new System.Drawing.Point(44, 12);
+            this.OpenBrowser.Name = "OpenBrowser";
+            this.OpenBrowser.Size = new System.Drawing.Size(107, 23);
+            this.OpenBrowser.TabIndex = 0;
+            this.OpenBrowser.Text = "Открыть Browser";
+            this.OpenBrowser.UseVisualStyleBackColor = true;
+            this.OpenBrowser.Click += new System.EventHandler(this.OpenBrowser_Click);
+            // 
+            // CloseBrowser
+            // 
+            this.CloseBrowser.BackColor = System.Drawing.Color.DimGray;
+            this.CloseBrowser.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.CloseBrowser.ForeColor = System.Drawing.Color.Maroon;
+            this.CloseBrowser.Location = new System.Drawing.Point(44, 177);
+            this.CloseBrowser.Name = "CloseBrowser";
+            this.CloseBrowser.Size = new System.Drawing.Size(107, 23);
+            this.CloseBrowser.TabIndex = 1;
+            this.CloseBrowser.Text = "Закрыть Browser";
+            this.CloseBrowser.UseVisualStyleBackColor = false;
+            this.CloseBrowser.Click += new System.EventHandler(this.CloseBrowser_Click);
+            // 
+            // FileSelectorURLs
+            // 
+            this.FileSelectorURLs.BackColor = System.Drawing.Color.DimGray;
+            this.FileSelectorURLs.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.FileSelectorURLs.ForeColor = System.Drawing.Color.Maroon;
+            this.FileSelectorURLs.Location = new System.Drawing.Point(44, 229);
+            this.FileSelectorURLs.Name = "FileSelectorURLs";
+            this.FileSelectorURLs.Size = new System.Drawing.Size(107, 23);
+            this.FileSelectorURLs.TabIndex = 2;
+            this.FileSelectorURLs.Text = "Выбрать файл\r\n";
+            this.FileSelectorURLs.UseVisualStyleBackColor = false;
+            this.FileSelectorURLs.Click += new System.EventHandler(this.FileSelectorURLs_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выбрать файл со списком товаров";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(638, 287);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileSelectorURLs);
+            this.Controls.Add(this.CloseBrowser);
+            this.Controls.Add(this.OpenBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AliBot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button OpenBrowser;
+        private System.Windows.Forms.Button CloseBrowser;
+        private System.Windows.Forms.Button FileSelectorURLs;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- AliBot/Form1.cs
+++ AliBot/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
 
 namespace AliBot
 {
     public partial class Form1 : Form
     {
+        IWebDriver browser;
+        string filePath;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void OpenBrowser_Click(object sender, EventArgs e)
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("http://ru.aliexpress.com/item/16708/32640351795.html?spm=2114.03010108.3.3.rg4xNc&ws_ab_test=searchweb201556_0,searchweb201602_4_10048_10057_10039_10047_10065_10056_10055_10037_10054_301_10046_10059_10045_10058_10017_107_10060_10061_10052_414_10062_10053_10050_10051,searchweb201603_1&btsid=b7723fb6-addf-4c48-952c-ce3d8ab26850");
+        }
+
+        private void CloseBrowser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                browser.Quit();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void FileSelectorURLs_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPD = new OpenFileDialog();
+            if (OPD.ShowDialog() == DialogResult.OK) {
+                filePath = OPD.FileName;
+            };
+            
         }
     }
 }
