927cabd1f2ffc8f8816c540d20cf7f7b2795157e
fieldyslap <fieldyslap@yandex.ru>
2016-08-30 00:14:54 +0700

    Mid

--- AliBot/Form1.Designer.cs
+++ AliBot/Form1.Designer.cs
@@ -34,11 +34,15 @@
             this.FileSelectorURLs = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.StartWork = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TimeOutSetter = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeOutSetter)).BeginInit();
             this.SuspendLayout();
             // 
             // OpenBrowser
             // 
-            this.OpenBrowser.Location = new System.Drawing.Point(44, 12);
+            this.OpenBrowser.Location = new System.Drawing.Point(44, 21);
             this.OpenBrowser.Name = "OpenBrowser";
             this.OpenBrowser.Size = new System.Drawing.Size(107, 23);
             this.OpenBrowser.TabIndex = 0;
@@ -51,9 +55,9 @@
             this.CloseBrowser.BackColor = System.Drawing.Color.DimGray;
             this.CloseBrowser.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.CloseBrowser.ForeColor = System.Drawing.Color.Maroon;
-            this.CloseBrowser.Location = new System.Drawing.Point(44, 177);
+            this.CloseBrowser.Location = new System.Drawing.Point(44, 221);
             this.CloseBrowser.Name = "CloseBrowser";
-            this.CloseBrowser.Size = new System.Drawing.Size(107, 23);
+            this.CloseBrowser.Size = new System.Drawing.Size(132, 54);
             this.CloseBrowser.TabIndex = 1;
             this.CloseBrowser.Text = "Закрыть Chrome";
             this.CloseBrowser.UseVisualStyleBackColor = false;
@@ -64,7 +68,7 @@
             this.FileSelectorURLs.BackColor = System.Drawing.Color.DimGray;
             this.FileSelectorURLs.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.FileSelectorURLs.ForeColor = System.Drawing.Color.Maroon;
-            this.FileSelectorURLs.Location = new System.Drawing.Point(44, 229);
+            this.FileSelectorURLs.Location = new System.Drawing.Point(44, 50);
             this.FileSelectorURLs.Name = "FileSelectorURLs";
             this.FileSelectorURLs.Size = new System.Drawing.Size(107, 23);
             this.FileSelectorURLs.TabIndex = 2;
@@ -75,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 234);
+            this.label1.Location = new System.Drawing.Point(157, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(186, 13);
             this.label1.TabIndex = 3;
@@ -84,19 +88,65 @@
             // StartWork
             // 
             this.StartWork.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.StartWork.Location = new System.Drawing.Point(44, 54);
+            this.StartWork.Location = new System.Drawing.Point(44, 168);
             this.StartWork.Name = "StartWork";
-            this.StartWork.Size = new System.Drawing.Size(107, 23);
+            this.StartWork.Size = new System.Drawing.Size(107, 47);
             this.StartWork.TabIndex = 4;
             this.StartWork.Text = "Сбор информации";
             this.StartWork.UseVisualStyleBackColor = false;
             this.StartWork.Click += new System.EventHandler(this.StartWork_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выбрать файл для сохранения результатов";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(44, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Выбрать файл\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // TimeOutSetter
+            // 
+            this.TimeOutSetter.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.TimeOutSetter.Location = new System.Drawing.Point(44, 119);
+            this.TimeOutSetter.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.TimeOutSetter.Name = "TimeOutSetter";
+            this.TimeOutSetter.Size = new System.Drawing.Size(120, 20);
+            this.TimeOutSetter.TabIndex = 7;
+            this.TimeOutSetter.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(638, 287);
+            this.Controls.Add(this.TimeOutSetter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.StartWork);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FileSelectorURLs);
@@ -105,6 +155,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "AliBot";
+            ((System.ComponentModel.ISupportInitialize)(this.TimeOutSetter)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +168,9 @@
         private System.Windows.Forms.Button FileSelectorURLs;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button StartWork;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown TimeOutSetter;
     }
 }
 
--- AliBot/Form1.cs
+++ AliBot/Form1.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
+using System.IO;
+using Microsoft.Office;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace AliBot
 {
@@ -17,10 +20,13 @@ namespace AliBot
     {
         IWebDriver browser;
         string filePath;
+        public int timeout;
+        public OpenFileDialog OPD = new OpenFileDialog();
 
         public Form1()
         {
-            InitializeComponent();            
+            InitializeComponent();
+            
         }
 
         private void OpenBrowser_Click(object sender, EventArgs e)
@@ -47,8 +53,7 @@ namespace AliBot
         }
 
         private void FileSelectorURLs_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog OPD = new OpenFileDialog();
+        {            
             if (OPD.ShowDialog() == DialogResult.OK) {
                 filePath = OPD.FileName;
                 label1.Text = filePath;
@@ -57,41 +62,46 @@ namespace AliBot
 
         private void StartWork_Click(object sender, EventArgs e)
         {
+            StreamReader urlsFiles;
+            string[] urls;
+            timeout = (int)TimeOutSetter.Value;
+
             try
             {
-                Thread.Sleep(1000);
-                List<string> values = new List<string>();
-                List<string> sizes = new List<string>();
-                List<string> features = new List<string>();
+                urlsFiles = new StreamReader(OPD.FileName);
+                urls = urlsFiles.ReadToEnd().Split('\n');                
                 browser.Navigate().GoToUrl("http://ru.aliexpress.com/");
-                Thread.Sleep(6000);
-                browser.Navigate().GoToUrl("http://ru.aliexpress.com/item/COLROVE-Work-Summer-Style-Women-Bodycon-Dresses-Sexy-2016-New-Arrival-Casual-Green-Crew-Neck-Half/32655446818.html?spm=2114.03010108.3.12.1ZdDIh&ws_ab_test=searchweb201556_0,searchweb201602_4_10048_10039_10057_10047_10065_10056_10055_10037_10054_10046_10059_10045_10032_10058_10017_107_10060_10061_10052_414_10062_10053_10050_10051,searchweb201603_1&btsid=3c650fc1-9996-4108-ba8f-9024881c6155");
-                Thread.Sleep(6000);
-                values.Add(browser.Title);
-                values.Add(browser.FindElement(By.Id("j-sku-discount-price")).Text);                
+                Thread.Sleep(timeout);
+                foreach (string currentURL in urls) {                     
+                    List<string> values = new List<string>();
+                    List<string> sizes = new List<string>();
+                    List<string> features = new List<string>(); 
+                    browser.Navigate().GoToUrl(currentURL);
+                    Thread.Sleep(timeout);
+                    values.Add(browser.Title);
+                    values.Add(browser.FindElement(By.Id("j-sku-discount-price")).Text);
+                    var sizesObjects = browser.FindElements(By.CssSelector("#j-sku-list-2 li a span"));
 
-                var sizesObjects = browser.FindElements(By.CssSelector("#j-sku-list-2 li a span"));
+                    foreach (var sizeVal in sizesObjects)
+                    {
+                        sizes.Add(sizeVal.Text);//Лист с размерами
+                    }
 
-                MessageBox.Show(sizesObjects[0].Text);
+                    var featuresObjeectsTitle = browser.FindElements(By.CssSelector("span.propery-title"));
+                    var featuresObjeectsDes = browser.FindElements(By.CssSelector("span.propery-des"));
+                    string[,] featuresEndValues = new string[featuresObjeectsTitle.Count, 2];
 
-                foreach (var sizeVal in sizesObjects)
-                {
-                    sizes.Add(sizeVal.Text);//Лист с размерами
+                    for (int i = 0; i < featuresObjeectsTitle.Count; i++)
+                    {
+                        featuresEndValues[i, 0] = featuresObjeectsTitle[i].Text;
+                        featuresEndValues[i, 1] = featuresObjeectsDes[i].Text;
+                        MessageBox.Show(featuresEndValues[i, 0] + " - " + featuresEndValues[i, 1]);
+                    }
+
+                    IWebElement imageToNextPage = browser.FindElement(By.CssSelector("a .ui-magnifier-glass"));
+                    imageToNextPage.Click();
+                    Thread.Sleep(timeout);        
                 }
-
-                var featuresObjeectsTitle = browser.FindElements(By.ClassName(""));
-                var featuresObjeectsDes = browser.FindElements(By.ClassName(""));
-                string[,] featuresEndValues = new string[featuresObjeectsTitle.Count, 2];
-
-                for (int i = 0; i < featuresObjeectsTitle.Count; i++)
-                {
-                    featuresEndValues[i, 0] = featuresObjeectsTitle[i].Text;
-                    featuresEndValues[i, 1] = featuresObjeectsDes[i].Text;
-                }
-
-                //IWebElement imageToNextPage = browser.FindElement(By.ClassName("a .ui-magnifier-glass"));
-                //imageToNextPage.Click();
-
             }
             catch (Exception ex)
             {
