1300f2997fb0632253fc0eaa4fd143565847086b
fieldyslap <fieldyslap@yandex.ru>
2016-08-31 19:21:10 +0700

    V 0.7b

--- AliBot/Form1.cs
+++ AliBot/Form1.cs
@@ -19,30 +19,49 @@ namespace AliBot
     public partial class Form1 : Form
     {
         IWebDriver browser;
+        IJavaScriptExecutor jse;
         string filePath;
         public int timeout;
         public OpenFileDialog OPD = new OpenFileDialog();
+        ChromeOptions chromeOptions;
+        StreamReader urlsFiles;
+        string[] urls;
+        string ProductTitle;
+        string ProductPrice;
+        public Excel.Application excelApp;
+        public Excel.Workbooks excelWB;
+        public Excel.Worksheet excelWSheets;
+        public Excel.Sheets excelSheets;
+        public Excel.Range ceels;    
+
+
+        List<string> colors = new List<string>(); // Лист с цветами 
+        List<string> sizes = new List<string>(); // Лист с Размерами
+        List<string> features = new List<string>(); // Лист с характеристиками
 
         public Form1()
         {
-            InitializeComponent();
-            
+            InitializeComponent();            
         }
 
         private void OpenBrowser_Click(object sender, EventArgs e)
         {
-            ChromeOptions chromeOptions = new ChromeOptions();
-            var prefs = new Dictionary<string, object> {
-                { "download.default_directory", @"C:\code" },
-                { "download.prompt_for_download", false }
-                };
-            chromeOptions.AddAdditionalCapability("chrome.prefs", prefs);
+            chromeOptions = new ChromeOptions();            
             browser = new OpenQA.Selenium.Chrome.ChromeDriver();
-            browser.Manage().Window.Maximize();   
+            browser.Manage().Window.Maximize();
+            excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelWB = excelApp.Workbooks;
+            excelApp.Workbooks.Open(@"C:\Users\Админ\Downloads\products-2016-08-29_1.xlsx");
+            excelWSheets = excelApp.ActiveSheet as Excel.Worksheet;
+            excelSheets = excelWB[1].Worksheets;
+
+
+            
         }
 
         private void CloseBrowser_Click(object sender, EventArgs e)
-        {
+        {            
             try
             {
                 browser.Quit();
@@ -61,31 +80,74 @@ namespace AliBot
         }
 
         private void StartWork_Click(object sender, EventArgs e)
-        {
-            StreamReader urlsFiles;
-            string[] urls;
+        {           
+
             timeout = (int)TimeOutSetter.Value;
+            jse = browser as IJavaScriptExecutor;            
 
             try
             {
                 urlsFiles = new StreamReader(OPD.FileName);
-                urls = urlsFiles.ReadToEnd().Split('\n');                
+                urls = urlsFiles.ReadToEnd().Split('\n');
+                urlsFiles.Close();              
                 browser.Navigate().GoToUrl("http://ru.aliexpress.com/");
                 Thread.Sleep(timeout);
-                foreach (string currentURL in urls) {                     
-                    List<string> values = new List<string>();
-                    List<string> sizes = new List<string>();
-                    List<string> features = new List<string>(); 
+                foreach (string currentURL in urls) {                   
                     browser.Navigate().GoToUrl(currentURL);
+                    Application.DoEvents();
                     Thread.Sleep(timeout);
-                    values.Add(browser.Title);
-                    values.Add(browser.FindElement(By.Id("j-sku-discount-price")).Text);
+                    ProductTitle = browser.Title; // Название товара
+
+                    try {
+                        ProductPrice = browser.FindElement(By.Id("j-sku-discount-price")).Text;
+                        if (ProductPrice != null)
+                        {
+                            if (ProductPrice.Contains("-"))
+                            {
+                                ProductPrice = ProductPrice.Split('-')[0];
+                            }
+                            if (ProductPrice.Contains("."))
+                            {
+                                ProductPrice = ProductPrice.Split('.')[0];
+                            }
+                        }
+                    }
+                    catch {
+                        ProductPrice = browser.FindElement(By.Id("j-sku-price")).Text;
+                        if (ProductPrice != null)
+                        {
+                            if (ProductPrice.Contains("-"))
+                            {
+                                ProductPrice = ProductPrice.Split('-')[0];
+                            }
+                            if (ProductPrice.Contains("."))
+                            {
+                                ProductPrice = ProductPrice.Split('.')[0];
+                            }
+                        }
+                    }
+
+                    try
+                    {
+                        var colorsElements = browser.FindElements(By.CssSelector("#j-product-info-sku dl dd ul li a img"));
+                        foreach (var el in colorsElements) {
+                            colors.Add(el.GetAttribute("title"));
+                            MessageBox.Show(el.GetAttribute("title"), "Заголовок");
+                        }
+                    }
+                    catch {
+
+                    }
+
+                    MessageBox.Show(ProductPrice, "Цена");
                     var sizesObjects = browser.FindElements(By.CssSelector("#j-sku-list-2 li a span"));
 
                     foreach (var sizeVal in sizesObjects)
                     {
-                        sizes.Add(sizeVal.Text);//Лист с размерами
+                        sizes.Add(sizeVal.Text);
+                        MessageBox.Show(sizeVal.Text, "Размеры");                      
                     }
+
 
                     var featuresObjeectsTitle = browser.FindElements(By.CssSelector("span.propery-title"));
                     var featuresObjeectsDes = browser.FindElements(By.CssSelector("span.propery-des"));
@@ -93,20 +155,42 @@ namespace AliBot
 
                     for (int i = 0; i < featuresObjeectsTitle.Count; i++)
                     {
-                        featuresEndValues[i, 0] = featuresObjeectsTitle[i].Text;
-                        featuresEndValues[i, 1] = featuresObjeectsDes[i].Text;
-                        MessageBox.Show(featuresEndValues[i, 0] + " - " + featuresEndValues[i, 1]);
+                        if (featuresObjeectsTitle[i].Text == "Материал:" || featuresObjeectsTitle[i].Text == "Длина платья:" || featuresObjeectsTitle[i].Text == "Сезон:" || featuresObjeectsTitle[i].Text == "Длина рукава:") {
+                            featuresEndValues[i, 0] = featuresObjeectsTitle[i].Text;
+                            featuresEndValues[i, 1] = featuresObjeectsDes[i].Text;
+                        }
+                                               
+                    }                    
+
+                    IWebElement imageToNextPage = browser.FindElement(By.CssSelector(".ui-image-viewer a.ui-magnifier-glass"));
+                    imageToNextPage.Click();
+                    Application.DoEvents();
+                    Thread.Sleep(timeout);                    
+                    
+                    var LIimagesForSave = browser.FindElements(By.CssSelector("ul.new-img-border li a img"));
+
+                    foreach (var str in LIimagesForSave) {
+                        Thread.Sleep(1000);
+                        jse.ExecuteScript(string.Format("var link = document.createElement('a'); link.target = '_blank'; link.download = 'img.jpg'; link.href = '{0}'; link.click();", str.GetAttribute("src")));                        
                     }
 
-                    IWebElement imageToNextPage = browser.FindElement(By.CssSelector("a .ui-magnifier-glass"));
-                    imageToNextPage.Click();
-                    Thread.Sleep(timeout);        
+                    sizes.Clear();
+                    features.Clear();
+
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
+        }
+
+        public void PrintingToExcel(string title, string price, List<string> sizes, List<string> colors, string[,] featuresEndValues) {
+            excelSheets = excelWB[0].Worksheets;
+            //Получаем ссылку на лист 1
+            excelWSheets = (Excel.Worksheet)excelSheets.get_Item(1);
+            //Выделение группы ячеек
+            excelWSheets.Rows.RowHeight = 260;
         }
     }
 }
